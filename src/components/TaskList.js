b8539b0ccb0f68d0637e63e3a5d0629a91e49bac
Igorinni <igortolmachyov7@gmail.com>
2022-11-23 11:59:49 +0300

    Rename class in TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,7 @@ function TaskList({arrayToDisplayTasks, deleteTask, checkTask}) {
  
     return (
       <div className="taskList">
-        {arrayToDisplayTasks.length < 1 && <p className="taskZero">No tasks</p>}
+        {arrayToDisplayTasks.length < 1 && <p className="noTasks">No tasks</p>}
         {arrayToDisplayTasks.map(elem => <Task task={elem} key={elem.id} deleteTask={deleteTask} checkTask={checkTask} />)}
       </div>
     )
