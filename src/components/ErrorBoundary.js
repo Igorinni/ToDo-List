1f80f8113fae3f2f298a18fd1ece505c9eeb639b
Igorinni <igortolmachyov7@gmail.com>
2022-11-28 23:38:02 +0300

    Make styles for ErrorBounndary.js

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, Heading, Accordion, AccordionItem, AccordionButton, AccordionIcon } from '@chakra-ui/react';
 
 class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -7,29 +8,45 @@ class ErrorBoundary extends React.Component {
     }
     
     componentDidCatch(error, errorInfo) {
-      // Catch errors in any components below and re-render with error message
       this.setState({
         error: error,
         errorInfo: errorInfo
       })
-      // You can also log error messages to an error reporting service here
     }
     
     render() {
       if (this.state.errorInfo) {
-        // Error path
         return (
-          <div className="errorBoundary">
-            <h2>Something went wrong.</h2>
-            <details style={{ whiteSpace: 'pre-wrap' }}>
-              {this.state.error && this.state.error.toString()}
-              <br />
-              {this.state.errorInfo.componentStack}
-            </details>
-          </div>
+          <Box my='100' mx='100' p='50' fontSize='20'
+          bg='rgba(250, 192, 3, 0.418)' color='rgb(197, 2, 2)'
+          borderRadius='15' display='flex' flexDirection='column'
+          alignItems='center' textAlign='center'
+          >
+            <Heading fontWeight='700' margin='5'>
+              Something went wrong.
+            </Heading>
+
+            <Accordion allowToggle maxW='500px'>
+              <AccordionItem display='flex' flexDirection='column' >
+                
+                <AccordionButton display='flex' justifyContent='center' mt='10' py='6' px='20'
+                bg='rgba(250, 192, 3, 0.418)' borderRadius='5'>
+                  <Box textAlign='center' fontSize='20' color='rgb(197, 2, 2)'>
+                    Details
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                
+                <AccordionPanel pt='5' display='flex' flexWrap='wrap'>
+                  {this.state.error && this.state.error.toString()}
+                  {this.state.errorInfo.componentStack}
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+
+          </Box> 
         );
       }
-      // Normally, just render children
       return this.props.children;
     }  
   }
